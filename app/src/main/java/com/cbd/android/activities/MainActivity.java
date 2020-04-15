00ad389df52535551857cc092e2396a323e1440e
David Gil Baeza <davidgil.univ@gmail.com>
2020-04-15 18:04:37 +0200

    Toolbar configurado

--- app/src/main/java/com/cbd/android/activities/MainActivity.java
+++ app/src/main/java/com/cbd/android/activities/MainActivity.java
@@ -14,6 +14,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -21,14 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         // Prueba
         Toast.makeText(MainActivity.this, Utils.getToken(), Toast.LENGTH_LONG).show();
