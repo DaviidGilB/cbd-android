33499872e200cc5c297e15b3f0bef2e2924550c0
David Gil Baeza <davidgil.univ@gmail.com>
2020-04-19 18:56:31 +0200

    Añadido botón cerrar sesión

--- app/src/main/java/com/cbd/android/activities/MainActivity.java
+++ app/src/main/java/com/cbd/android/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.cbd.android.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,9 +21,11 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private BottomNavigationView navView;
     private FloatingActionButton fab;
+    private ImageView appExitImage;
+    private TextView appExitText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         fab = findViewById(R.id.fab);
+        appExitImage = findViewById(R.id.app_exit);
+        appExitText = findViewById(R.id.app_exit_text);
 
         // Carga del fragment
         getSupportFragmentManager().beginTransaction()
@@ -41,8 +49,6 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.navigation_home:
-                                return true;
-                            case R.id.navigation_my_posts:
                                 return true;
                             case R.id.navigation_my_profile:
                                 return true;
@@ -59,15 +65,46 @@ public class MainActivity extends AppCompatActivity {
                 dialogFragment.show(getSupportFragmentManager(), "NewPostDialogFragment");
             }
         });
+
+        // Listener exit
+        appExitText.setOnClickListener(this);
+        appExitImage.setOnClickListener(this);
+
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        Utils.deleteToken();
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
         finish();
     }
 
+    private void exit() {
+        Utils.deleteToken();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Integer id = v.getId();
+        if (id == R.id.app_exit_text || id == R.id.app_exit) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("¿Desea realmente salir de la aplicación?").setTitle("Cerrar sesión");
+            builder.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    exit();
+                }
+            });
+            builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
 }
